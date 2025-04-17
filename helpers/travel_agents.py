1433445b842dc4e56971966e37ff8795f754689d
malinphy <mehmetalinebioglu@gmail.com>
2025-04-18 00:00:55 +0300

    refactor

--- helpers/travel_agents.py
+++ helpers/travel_agents.py
@@ -6,8 +6,8 @@ from agents.model_settings import ModelSettings
 import nest_asyncio
 from data_models.first_question_output_format import FinalModel
 import datetime
-from tools import flight_search, yelp_search, hotels_search, wikipedia_search, get_weather
-
+from helpers.tools import flight_search, yelp_search, hotels_search, wikipedia_search, get_weather
+from helpers.function_tools import flight_search_2, hotels_search2, yelp_search2
 
 question_rewrite_agent = Agent(
     name = "Customer Help",
@@ -27,10 +27,8 @@ Try to extract the search terms from the question
     output_type = FinalModel
 )
 
-
-flight_agent = Agent(
-    name = "Google flight information",
-    instructions= """
+f_agent = Agent(name="Flight Assistant agent",
+              instructions="""
 Always answer in Turkish language. Return all the given flight information
 
 departure_id : Parameter defines the departure airport code or location kgmid. An airport code is an uppercase 3-letter code. For example, CDG is Paris Charles de Gaulle Airport and AUS is Austin-Bergstrom International Airport.
@@ -41,10 +39,53 @@ outbound_date : Parameter defines the outbound date. The format is YYYY-MM-DD. e
 
 return_date : Parameter defines the return date. The format is YYYY-MM-DD. e.g. 2025-04-15
 """,
+              model="gpt-4o-mini",
+              # model = 'gpt-4o',
+              model_settings=ModelSettings(temperature= 0.0,
+                                 max_tokens = 4096*8),
+            tools= [flight_search_2]
+              )
+
+# h_agent 
+h_agent = Agent(
+    name = "Google hotels information",
+    instructions= """Returns google hotels information as a json.     
+    q : Search Query
+    gl : Country""",
     model = 'gpt-4o-mini',
-    tools=[flight_search],
-    # model_settings={"temperature": 0.0}
-    # output_type = first_agent_output
+    tools=[hotels_search2],
     model_settings=ModelSettings(temperature= 0.0,
-                                 max_tokens = 4096*2)
+                                 max_tokens = 4096*2),
+)
+
+
+y_agent = Agent(
+    name = "Yelp Search information",
+    instructions= """Returns Yelp search results.     
+    search_term: str, 
+    location: str
+    """.strip(),
+    model = 'gpt-4o-mini',
+    tools=[yelp_search2],
+    model_settings=ModelSettings(temperature= 0.0,
+                                 max_tokens = 4096*2),
+)
+
+t_agent = Agent(
+    name = "Travel Assistant agent",
+    instructions = """ 
+Answer the users questions
+""",
+model = 'gpt-4o-mini',
+   tools=[
+       f_agent.as_tool(
+           tool_name="flight_information",
+           tool_description="Return flight information according to questions"
+       ),
+       h_agent.as_tool(
+           tool_name="hotel_information",
+           tool_description="Return hotel information according to questions"
+       )
+   ]
+
 )
