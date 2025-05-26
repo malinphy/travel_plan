a03966aea6765b305ed53058f6b997b7e0fc31f6
malinphy <mehmetalinebioglu@gmail.com>
2025-05-26 10:11:11 +0300

    context_models

---
+++ data_models/context_models.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel, Field
+from typing import List,Optional
+
+
+class Flight(BaseModel):
+    departure_id: Optional[List[str]] = Field(
+        None, description="List of departure airport or city, IDs."
+    )
+    arrival_id: Optional[List[str]] = Field(
+        None, description="List of arrival airport or city, IDs."
+    )
+    outbound_date: Optional[List[str]] = Field(
+        None, description="List of outbound (departure) dates in YYYY-MM-DD format."
+    )
+    return_date: Optional[List[str]] = Field(
+        None, description="List of return dates in YYYY-MM-DD format."
+    )
+
+
+class Hotel(BaseModel):
+    q: Optional[List[str]] = Field(
+        None, description="List of hotel search queries or names."
+    )
+    check_in_date: Optional[List[str]] = Field(
+        None, description="List of check-in dates in YYYY-MM-DD format."
+    )
+    check_out_date: Optional[List[str]] = Field(
+        None, description="List of check-out dates in YYYY-MM-DD format."
+    )
+    gl: Optional[List[str]] = Field(
+        None, description="List of locations or geolocations for the hotel search."
+    )    
+
+
+class Yelp(BaseModel):
+    search_term: Optional[List[str]] = Field(
+        None, description="List of search terms for restaurants or businesses."
+    )
+    location: Optional[List[str]] = Field(
+        None, description="List of locations for the Yelp search."
+    )
+
+class TotalModel(BaseModel):
+    hotel: Optional[Hotel] = None
+    flight: Optional[Flight] = None
+    restaurant : Optional[Yelp] = None        
