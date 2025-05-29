e28bb485b6ad4588e6660e722d8a34484d7aeadf
malinphy <mehmetalinebioglu@gmail.com>
2025-05-29 15:34:40 +0300

    data_models

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -5,3 +5,17 @@ def assemble_conversation(result, new_input):
     else:
         new_input = new_input
     return new_input
+
+
+def text_maker(res):
+    step_agents = ""
+    for i in (res.final_output.__dict__['sequence']):
+        exp = f"Step : {i.step.explanation}"
+        step_agents += f"Step : {i.step.explanation}"
+        if hasattr(i, 'agents') and i.agents is not None and len(i.agents) > 0:
+            agent_text = f"Agent : {i.agents[0].name}"
+            step_agents += f" Agent : {i.agents[0].name} \n"
+        else:
+            agent_text = "Agent : No agents assigned for this step."
+            step_agents += " Agent : No agents assigned for this step.\n"
+    return step_agents
